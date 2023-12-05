922e7f944371617010f3d128cdf61cfc17e3dd88
Shujat Ali <Shujat.dev@gmail.com>
2023-12-05 10:36:20 -0600

    added function to disable copy paste

--- Sources/MyFramework/MyFramework.swift
+++ Sources/MyFramework/MyFramework.swift
@@ -1,6 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import UIKit
+
 public class MyFramework {
     
     public init() {
@@ -9,5 +11,12 @@ public class MyFramework {
     
     public func enableCopyPaste() {
         
+    }
+}
+
+
+extension UITextView {
+    open override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        return false
     }
 }
