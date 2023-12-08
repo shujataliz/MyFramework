a272bb5d6e1db5e9970970650e6eb7921ff08b50
Shujat Ali <Shujat.dev@gmail.com>
2023-12-08 10:37:25 -0600

    added mirror helper

--- Package.swift
+++ Package.swift
@@ -10,17 +10,12 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "MyFramework",
-            targets: ["MyFramework"]),
+        .library(name: "MyFramework", targets: ["MyFramework"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MyFramework"),
-        .testTarget(
-            name: "MyFrameworkTests",
-            dependencies: ["MyFramework"]),
+        .target(name: "MyFramework", dependencies: []),
+        .testTarget(name: "MyFrameworkTests", dependencies: ["MyFramework"]),
     ]
 )
